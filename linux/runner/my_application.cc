c08d2d9dfdd7c06319652b1ae5ec61f4a11b36df
JP Duplessis <jp@vteam.com>
2025-09-11 11:32:43 +0100

    add App Icon

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -54,6 +54,7 @@ static void my_application_activate(GApplication* application) {
   }
 
   gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_icon_from_file(GTK_WINDOW(window),"assets/app.png",NULL);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
