736b55c51dab1508bedb94b7d88899029fabbbd4
JP Duplessis <jp@vteam.com>
2025-10-04 17:26:02 +0100

    run_test -  quiet output

--- tool/run_test.sh
+++ tool/run_test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+clear
+
 # Run unit tests
 flutter test --coverage --coverage-path=coverage/unit.info
 
@@ -10,9 +12,9 @@ flutter test integration_test --coverage --coverage-path=coverage/integration.in
 # Merge coverage
 lcov --add-tracefile coverage/unit.info \
      --add-tracefile coverage/integration.info \
-     --output-file coverage/lcov.info
+     --output-file coverage/lcov.info > /dev/null 2>&1
 
 # Generate HTML report
-genhtml coverage/lcov.info -o coverage/html
+genhtml coverage/lcov.info -o coverage/html > /dev/null 2>&1
 
 open coverage/html/index.html
